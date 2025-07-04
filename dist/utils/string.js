8ac49f6f4d1f9a81c4267847a7f79b6256729ec4
Louis Johnson <louiszn269@gmail.com>
2025-07-04 14:22:08 +0700

    Add dist to .gitignore

--- dist/utils/string.js
+++
@@ -1,7 +0,0 @@
-export function sanitize(input) {
-    return input.replace(/[^\w]/g, "");
-}
-export function extractId(input) {
-    const match = input.match(/\d{17,20}/);
-    return match ? match[0] : null;
-}
