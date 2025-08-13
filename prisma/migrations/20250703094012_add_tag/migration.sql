3aa7e14ebf9ad91c4be28f2d3be5138733c6810c
Louis Johnson <louiszn269@gmail.com>
2025-08-13 23:24:50 +0700

    chore: remove prisma completely

--- prisma/migrations/20250703094012_add_tag/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Character" ADD COLUMN     "tag" TEXT NOT NULL DEFAULT 'temp-tag',
-ALTER COLUMN "name" DROP NOT NULL;
--- prisma/migrations/20250703101458_remove_default_value/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Character" ALTER COLUMN "tag" DROP DEFAULT;
