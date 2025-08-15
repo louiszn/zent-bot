82923326d2f64246c27c24db0a46a2a54ec18bcb
Louis Johnson <louiszn269@gmail.com>
2025-08-15 15:30:04 +0700

    chore: convert eslint to typescript and stricter type check

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,28 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
 	eslint.configs.recommended,
-	tseslint.configs.recommended,
+	tseslint.configs.recommendedTypeChecked,
 	{
-		ignores: ["dist/**/*"],
+		languageOptions: {
+			parserOptions: {
+				project: "./tsconfig.eslint.json",
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
 	},
 	{
-		files: ["src/**/*.ts"],
+		ignores: ["dist/**/*", "node_modules/**/*"],
+	},
+	{
+		files: ["**/*.ts"],
 		rules: {
 			semi: "error",
 			quotes: ["error", "double"],
 			indent: ["error", "tab", { SwitchCase: 1 }],
 			"@typescript-eslint/consistent-type-imports": "error",
+			"@typescript-eslint/no-floating-promises": "error",
 		},
 	},
 );
